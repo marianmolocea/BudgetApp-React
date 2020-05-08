8b1cdf372ba0b414f8e28d24c8ed8168cd62a3af
marianmolocea <marian.mcrt@gmail.com>
2020-05-08 16:57:48 +0100

    implement the login route

--- src/backend/userRoutes/userRoutes.js
+++ src/backend/userRoutes/userRoutes.js
@@ -3,5 +3,6 @@ const authHandler = require('../users/authHandler');
 
 const router = express.Router();
 router.post('/signup', authHandler.signUp);
+router.post('/login', authHandler.logIn);
 
 module.exports = router;
--- src/backend/users/authHandler.js
+++ src/backend/users/authHandler.js
@@ -1,15 +1,21 @@
 const User = require('./usersModel');
+const jwt = require('jsonwebtoken');
+const AppError = require('../appError')
 
 exports.signUp = async (req, res) => {
     try {
         const newUser = await User.create({
-            email: req.body.name,
+            email: req.body.email,
             password: req.body.password,
             confirmPassword: req.body.confirmPassword
         });
-        
+
+        const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET, {
+            expiresIn: process.env.JWT_EXPIRES_IN
+        });
         res.status(201).json({
             status: "success",
+            token,
             data: {
                 user: newUser
             }
@@ -20,5 +26,18 @@ exports.signUp = async (req, res) => {
             message: err
         })
     }
+}
+
+exports.logIn = (req, res, next) => {
+    const {email, password} = req.body;
+
+    if(!email || !password) {
+        return next(new AppError('Please provide the user and password', 400))
+    };
     
+    const token = ''
+    res.status(200).json({
+        status: 'success',
+        token
+    })
 }
